d468b166987cf14e3008cf21a1f30e1f059a6a1c
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-02-24 07:19:55 +0700

    update homepage

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect, SetStateAction } from 'react'
+import Navbar from '../components/Navbar'
 import PokeCard from '../components/PokeCard'
 
 const Homepage = () => {
-       // const pokemonapi = "https://pokeapi.co/api/v2/pokemon"
+       const pokemonapi = "https://pokeapi.co/api/v2/pokemon"
 
        const [pokemon, setPokemon]: SetStateAction<any> = useState([])
-       const [loadMore, setLoadMore] = useState("https://pokeapi.co/api/v2/pokemon?limit=20")
+       const [loadMore, setLoadMore] = useState(`${pokemonapi}?limit=20`)
 
        const fetchPokemon = async () => {
               const response = await fetch(loadMore)
@@ -15,14 +16,13 @@ const Homepage = () => {
 
               const createPokemon = (results: any) => {
                      results.forEach(async (pokemon: any) => {
-                            const responses = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+                            const responses = await fetch(`${pokemonapi}/${pokemon.name}`)
                             const dataPokemonName = await responses.json()
                             // console.log(dataPokemonName)
 
                             setPokemon((currentList: any[]) => [...currentList, dataPokemonName])
                      })
               }
-
               createPokemon(data.results)
               await console.log(pokemon)
        }
@@ -33,9 +33,10 @@ const Homepage = () => {
 
        return (
               <React.Fragment>
+                     <Navbar />
                      <h1 className='font-bold text-2xl text-white text-center'>Pokedeck with Pokemon</h1>
-                     <div className="mx-10 my-10">
-                            <div className="grid grid-cols-5 gap-5">
+                     <div className="mx-6 md:mx-10 my-10">
+                            <div className="grid grid-cols-2 gap-3 md:grid-cols-3 md:gap-3 lg:grid-cols-5 lg:gap-5">
                                    {pokemon.map((pokemonItem: any, index: number) => (
                                           <PokeCard
                                                  id={pokemonItem.id}
@@ -45,6 +46,9 @@ const Homepage = () => {
                                                  key={index}
                                           />
                                    ))}
+                            </div>
+                            <div className="text-center">
+                                   <button onClick={() => fetchPokemon()} className="py-2 text-white mt-4 px-6 border-none rounded-md bg-blue-600 font-medium text-md">Load more</button>
                             </div>
                      </div>
               </React.Fragment>
